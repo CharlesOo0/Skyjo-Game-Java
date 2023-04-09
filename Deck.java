55a5a1c59de10d2129334d70bf8fadc31b109355
CharlesOo0 <104007616+CharlesOo0@users.noreply.github.com>
2023-04-09 19:35:41 +0200

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -1,114 +1,72 @@
 package skyjo;
 
-import java.util.LinkedList;
-import java.util.Random;
+import java.util.Stack;
 
 /*!
- * @brief Class that implement the deck in the game
+ * @brief Class that implement the discard in the game
+ * 
  * */
 
-public class Deck extends LinkedList<CardElement> {
-
-	private static final long serialVersionUID = 1L;
-	private int deck_card; // Number of card in the deck
-	private Random rand; // Pick random number
+public class Discard {
+	private Stack<Integer> stack;
 	
-	/*---------------- Constructors ----------------*/
-	Deck() {
-		super();
-		this.deck_card = 150; // There are 150 card in the game
-		rand = new Random();
-		
-		for (int i = -2; i < 13; i++) { // Initialise a default deck
-			super.add(this.createCard(i)); // Go through all the possible card in the game and increment the default CardElemet
-		}								   // relative to them
+	/* Constructors */
+	Discard() {
+		this.stack = new Stack<Integer>(); // Initialise our stack
 	}
 	
-	/*---------------- Getters ----------------*/
+	/* Methods */
 	
-	public int getDeckCard() {
-		return this.deck_card;
-	}
-	
-	/*---------------- Methods ----------------*/
-	
-	/*! @brief : this method check if the deck is empty
-	 */
-	public boolean isEmpty() {
-		if (this.deck_card == 0) { // If there is no more card the deck is empty
-			return true;
-		}
-		
-		return false;
-	}
-	
-	
-	/*! @brief : this method facilitate the initialisation of the default deck
-	 * by returning the default CardElement relative to a card i.e. card = -2 , CardElement = {-2,5}
-	 */
-	public CardElement createCard(int card) {
-		CardElement obj = new CardElement(card);
-		return obj;
-	}
-	
-	/*! @brief : check if our linked list still contain copy of a precise card
-	 */
-	public int contain(int card) {
-		
-		for(int i = 0; i < super.size();  i++) { // Iterate over the element of the linked list
-			if (super.get(i).getCard() == card) { // If we find the card
-				return i; // then we return the index
-			}
-		}
-		
-		return -1; // else we return -1, that is equal to "card not found"
-	}
-	
-	
-	/*! @brief : toString method that return a string with the number of copy remaining of each card
-	 */
-	public String toString() {
-		String output = new String();
-		for(int i = 0; i < super.size(); i++) {
-			output += " " + super.get(i).getCard() + " " + super.get(i).getCardNumber() + "\n";
-		}
-		return output;
-	}
-	
-	
-	/*! @brief : Function that draw randomly a card in the deck
-	 */
-	public int draw() {
-		
-		if (deck_card == 0) { // If there are no more card in the deck we return 999 wich represent an order to stop our round
+	/*!
+	 * @brief : Return the value on top of the stack
+	 * */
+	public int peek() {
+		if(stack.isEmpty()) {
 			return 999;
 		}
 		
-		int cardPicked = rand.nextInt(1,deck_card + 1); // Pick a random number between 1 and the number of card in the deck
-		CardElement chosedCard;
-		chosedCard = super.get(cardPicked <= super.get(0).getCardNumber() ? 0:
-				cardPicked <= super.get(0).getCardNumber() + super.get(1).getCardNumber() ? 1:
-				cardPicked <= super.get(0).getCardNumber() + super.get(1).getCardNumber() + super.get(2).getCardNumber() ? 2:
-				cardPicked <= super.get(0).getCardNumber() + super.get(1).getCardNumber() + super.get(2).getCardNumber() + super.get(3).getCardNumber() ? 3:
-				cardPicked <= super.get(0).getCardNumber() + super.get(1).getCardNumber() + super.get(2).getCardNumber() + super.get(3).getCardNumber() + super.get(4).getCardNumber() ? 4:
-				cardPicked <= super.get(0).getCardNumber() + super.get(1).getCardNumber() + super.get(2).getCardNumber() + super.get(3).getCardNumber() + super.get(4).getCardNumber() + super.get(5).getCardNumber() ? 5:
-				cardPicked <= super.get(0).getCardNumber() + super.get(1).getCardNumber() + super.get(2).getCardNumber() + super.get(3).getCardNumber() + super.get(4).getCardNumber() + super.get(5).getCardNumber() + super.get(6).getCardNumber() ? 6:
-				cardPicked <= super.get(0).getCardNumber() + super.get(1).getCardNumber() + super.get(2).getCardNumber() + super.get(3).getCardNumber() + super.get(4).getCardNumber() + super.get(5).getCardNumber() + super.get(6).getCardNumber() + super.get(7).getCardNumber() ? 7:
-				cardPicked <= super.get(0).getCardNumber() + super.get(1).getCardNumber() + super.get(2).getCardNumber() + super.get(3).getCardNumber() + super.get(4).getCardNumber() + super.get(5).getCardNumber() + super.get(6).getCardNumber() + super.get(7).getCardNumber() + super.get(8).getCardNumber() ? 8:
-				cardPicked <= super.get(0).getCardNumber() + super.get(1).getCardNumber() + super.get(2).getCardNumber() + super.get(3).getCardNumber() + super.get(4).getCardNumber() + super.get(5).getCardNumber() + super.get(6).getCardNumber() + super.get(7).getCardNumber() + super.get(8).getCardNumber() + super.get(9).getCardNumber() ? 9:
-				cardPicked <= super.get(0).getCardNumber() + super.get(1).getCardNumber() + super.get(2).getCardNumber() + super.get(3).getCardNumber() + super.get(4).getCardNumber() + super.get(5).getCardNumber() + super.get(6).getCardNumber() + super.get(7).getCardNumber() + super.get(8).getCardNumber() + super.get(9).getCardNumber() + super.get(10).getCardNumber() ? 10:
-				cardPicked <= super.get(0).getCardNumber() + super.get(1).getCardNumber() + super.get(2).getCardNumber() + super.get(3).getCardNumber() + super.get(4).getCardNumber() + super.get(5).getCardNumber() + super.get(6).getCardNumber() + super.get(7).getCardNumber() + super.get(8).getCardNumber() + super.get(9).getCardNumber() + super.get(10).getCardNumber() + super.get(11).getCardNumber() ? 11:
-				cardPicked <= super.get(0).getCardNumber() + super.get(1).getCardNumber() + super.get(2).getCardNumber() + super.get(3).getCardNumber() + super.get(4).getCardNumber() + super.get(5).getCardNumber() + super.get(6).getCardNumber() + super.get(7).getCardNumber() + super.get(8).getCardNumber() + super.get(9).getCardNumber() + super.get(10).getCardNumber() + super.get(11).getCardNumber() + super.get(12).getCardNumber() ? 12:
-				cardPicked <= super.get(0).getCardNumber() + super.get(1).getCardNumber() + super.get(2).getCardNumber() + super.get(3).getCardNumber() + super.get(4).getCardNumber() + super.get(5).getCardNumber() + super.get(6).getCardNumber() + super.get(7).getCardNumber() + super.get(8).getCardNumber() + super.get(9).getCardNumber() + super.get(10).getCardNumber() + super.get(11).getCardNumber() + super.get(12).getCardNumber() + super.get(13).getCardNumber() ? 13:
-				cardPicked <= super.get(0).getCardNumber() + super.get(1).getCardNumber() + super.get(2).getCardNumber() + super.get(3).getCardNumber() + super.get(4).getCardNumber() + super.get(5).getCardNumber() + super.get(6).getCardNumber() + super.get(7).getCardNumber() + super.get(8).getCardNumber() + super.get(9).getCardNumber() + super.get(10).getCardNumber() + super.get(11).getCardNumber() + super.get(12).getCardNumber() + super.get(13).getCardNumber() + super.get(14).getCardNumber() ? 14:		
-				null); // Pick a random card in the deck
-		
-		
-		chosedCard.lowerCard(); // Lower the number of copy of the card picked
-		this.deck_card -= 1; // Lower the number of card in the deck
-		
-		return chosedCard.getCard(); // Return the card picked
-		
+		return stack.peek();
 	}
-
+	
+	/*!
+	 * @brief : Check if our list is empty
+	 * */
+	public boolean isEmpty() {
+		return this.stack.isEmpty(); // Check if stack is empty
+	}
+	
+	
+	/*!
+	 * @brief : Add an element to our list
+	 * */
+	public void push(int card) {
+		this.stack.push(card); // Add a card to the discard
+		return;
+	}
+	
+	
+	/*!
+	 * @brief : Pop the last element of our list
+	 * Only do it if the list is not empty
+	 * */
+	public int draw() {
+		if (this.isEmpty()) { // If stack empty we can not pop 
+			return -1; // So we return an error
+		}
+		
+		return this.stack.pop(); // Remove the last element of the stack and return it
+	}
+	
+	/*!
+	 * @brief : A to string method
+	 * The aim is to display easily our stack
+	 * */
+	public String toString() { 
+		String output = new String(); // Initalise output
+		for (int i = 0; i < this.stack.size(); i++) { // Iterate over the pile
+			output += " " + this.stack.get(i); // Increment our output to the output
+		}
+		return output; // Return all the element
+	}
+	
 }
