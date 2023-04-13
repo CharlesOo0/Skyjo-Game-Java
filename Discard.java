6b3e34771edb674c16827427ee016b31dd53dee9
CharlesOo0 <104007616+CharlesOo0@users.noreply.github.com>
2023-04-13 20:33:31 +0200

    Update Discard.java

--- Discard.java
+++ Discard.java
@@ -17,9 +17,12 @@ public class Discard {
 	
 	/* Methods */
 	
-	/*!
-	 * @brief : Return the value on top of the stack
-	 * */
+	/*! @brief : Return the value on top of the stack
+	 *
+	 * Behavior :
+	 * Use the isEmpty method natively present with the type Stack if it's empty return 999, that's represent an error.
+	 * Else just return the peek of the stack with the peek method natively present with the Stack.
+	 */
 	public int peek() {
 		if(stack.isEmpty()) {
 			return 999;
@@ -28,9 +31,11 @@ public class Discard {
 		return stack.peek();
 	}
 	
-	/*!
-	 * @brief : Check if our list is empty
-	 * */
+	/*! @brief : Check if our list is empty
+	 *  
+	 * Behavior : 
+	 * Use the native method isEmpty of Stack type and return it.
+	 */
 	public boolean isEmpty() {
 		return this.stack.isEmpty(); // Check if stack is empty
 	}
@@ -38,29 +43,37 @@ public class Discard {
 	
 	/*!
 	 * @brief : Add an element to our list
-	 * */
+	 * 
+	 * Behavior :
+	 * Add a card to the discard with the method push that is native to Stack type.
+	 */
 	public void push(int card) {
 		this.stack.push(card); // Add a card to the discard
 		return;
 	}
 	
 	
-	/*!
-	 * @brief : Pop the last element of our list
-	 * Only do it if the list is not empty
-	 * */
+	/*! @brief : Pop the last element of our list
+	 *  Only do it if the list is not empty
+	 *
+	 * Behavior :
+	 * Check if the stack is empty if so we return an error 999.
+	 * Else we pop the stack with native method of Stack.
+	 */
 	public int draw() {
 		if (this.isEmpty()) { // If stack empty we can not pop 
-			return -1; // So we return an error
+			return 999; // So we return an error
 		}
 		
 		return this.stack.pop(); // Remove the last element of the stack and return it
 	}
 	
-	/*!
-	 * @brief : A to string method
-	 * The aim is to display easily our stack
-	 * */
+	/*! @brief : A to string method, the aim is to display easily our stack
+	 *
+	 * Behavior :
+	 * Iterate over the stack with an index from 0 to size of the stack.
+	 * And increment value we get from the Stack with a get method to an output string.
+	 */
 	public String toString() { 
 		String output = new String(); // Initalise output
 		for (int i = 0; i < this.stack.size(); i++) { // Iterate over the pile
