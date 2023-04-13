a4d3fd01db91012267909d0f1ed09223dd2a1c03
CharlesOo0 <104007616+CharlesOo0@users.noreply.github.com>
2023-04-13 19:49:37 +0200

    Update BoardArray.java

--- BoardArray.java
+++ BoardArray.java
@@ -32,6 +32,13 @@ public class BoardArray extends LinkedList<BoardSet> {
 	/*---------------- Methods ----------------*/
 	
 	/*! @brief : Reverse a card from a specific boardSet
+	 *  @param indexRow int that represent the row of the card the player picked
+	 *  @param indexColumn int that represent the column of the card the player picked
+	 *  @param player in that represent the index of the player
+	 *
+	 * Behavior :
+	 * This method reverse a card of a specific player's board.
+	 * If the program fail it return false, else it return true.
 	 */
 	public boolean cardReverse(int indexRow, int indexColumn, int player) {
 		if (indexColumn >= super.get(player).getColumn() || indexColumn < 0 || indexRow >= 4 || indexRow < 0) {
@@ -46,6 +53,12 @@ public class BoardArray extends LinkedList<BoardSet> {
 
 	
 	/*! @brief : Check if one of the boardSet got all his card reversed
+	 *
+	 * Behavior :
+	 * This method iterate over every player's board. And it call an other
+	 * method boardNotHidden that will check if the selected board got all his card
+	 * reverse or not. If it encounter a board that got all his card reverse the method
+	 * return true else false.
 	 */
 	public boolean isNotHidden() {
 		for (int i = 0; i < nbPlayer ; i++) { // Iterate over the linked list of boards set
@@ -55,10 +68,17 @@ public class BoardArray extends LinkedList<BoardSet> {
 		}
 		return false;
 	}
+	
 	/*! @brief : The goal of this method is to print a specific board
+	 *  @param index int that represent the player we want to print the board
+	 *
+	 * Behavior : 
+	 * Check if the index entered is correct. If not end the program.
+	 * Then simply use the method displayBoardSet() from BoardSet to
+	 * display the player's board.
 	 */
 	public void displayTargetBoard(int index) {
-		if ( index < 0 || index >= 8) { // Acquisition control
+		if ( index < 0 || index >= nbPlayer) { // Acquisition control
 			System.out.println("Error index out of range");
 			return;
 		}
@@ -67,7 +87,11 @@ public class BoardArray extends LinkedList<BoardSet> {
 	}
 	
 	/*! @brief : The aim of this method is to display the board of all the player present
-	 * */
+	 *  
+	 * Behavior :
+	 * Iterate over the index of every player, and simply print the boards of every players.
+	 * With the method displayBoardSet() from BoardSet.
+	 */
 	public void displayBoards() {
 		for(int i = 0; i < nbPlayer; i++) { // Iterate over the BoardSet LinkedList
 			System.out.println("Player " + (i+1) + " board :");
